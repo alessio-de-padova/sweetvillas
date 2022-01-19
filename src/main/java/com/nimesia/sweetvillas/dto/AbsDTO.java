731704ac0b4a8e5584e6b948b5e5bbf1167e7a75
europe-for-everyone <57996166+europe-for-everyone@users.noreply.github.com>
2022-01-19 22:32:41 +0100

    Update AbsDTO.java

--- src/main/java/com/nimesia/sweetvillas/dto/AbsDTO.java
+++ src/main/java/com/nimesia/sweetvillas/dto/AbsDTO.java
@@ -1,11 +1,5 @@
 package com.nimesia.sweetvillas.dto;
 
-public class AbsDTO  {
+public class AbsDTO {
 
-    private static final String SECRET = "!z%C*F-JaNdRgUkXp2asdrtgfs5vx/A?D(G+Kb";
-
-
-    public static String getSECRET() {
-        return SECRET;
-    }
 }
