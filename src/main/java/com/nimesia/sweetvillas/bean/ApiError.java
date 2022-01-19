14d06003ae2a409cbd353cc99654a174eebb07a0
europe-for-everyone <57996166+europe-for-everyone@users.noreply.github.com>
2022-01-19 21:36:14 +0100

    Update ApiError.java

--- src/main/java/com/nimesia/sweetvillas/bean/ApiError.java
+++ src/main/java/com/nimesia/sweetvillas/bean/ApiError.java
@@ -1,21 +1,13 @@
 package com.nimesia.sweetvillas.bean;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.Bean;
-
 import java.io.Serializable;
 
 public class ApiError implements Serializable {
 
-    private ObjectMapper mapper;
     private String type;
     private String propertyPath;
     private String invalidValue;
 
-    @Bean
-    public ApiError ApiError() {
-        return new ApiError();
-    }
 
     public void setInvalidValue(String invalidValue) {
         this.invalidValue = invalidValue;
